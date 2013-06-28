27c35c1abab00d6d300ad056c4427b1821840909
curl.nicholas@gmail.com <curl.nicholas@gmail.com>
2013-06-28 09:50:37 -0500

    la

--- Final Project/Final Project Dragon/Final Project Dragon/Dragon_main.cpp
+++ Final Project/Final Project Dragon/Final Project Dragon/Dragon_main.cpp
@@ -18,26 +18,46 @@ void main()
 	cout << "Do want to play or exit? (y/n): ";
 	cin >> askplay;
 
-	if(askplay == "y")
+	isPlaying = (askplay == "y");
+
+	if(!isPlaying)
 	{
-		isPlaying = true;
-	}
-	else
-	{
-		isPlaying = false;
+		return;
 	}
 
-	if(isPlaying)
+	string pupname;
+	system("CLS");
+	cout << "You recieve a puppy.  Please name the puppy"<< endl;
+	cin >> pupname;
+
+	Dog* pup = new Dog();
+	pup->age = .083;
+	pup->energyLevel = 10;
+	pup->happiness =100;
+	pup->weight = 10;
+	pup->hunger = 100;
+	pup->name = pupname;
+
+	do
 	{
-		string pupname;
-		system("CLS");
-		cout << "You recieve a puppy.  Please name the puppy"<< endl;
-		cin >> pupname;
-		Dog* pup = new Dog();
-		pup->age = .083;
-		pup->energyLevel = 10;
-		pup->happiness =100;
-		pup->weight = 10;
-		pup->hunger = 100;
-	}
+		int selection;
+		cout << "1. Check on "<< pup->name << endl;
+		cout << "2. Interact with " << pup->name << endl;
+		cout << "3. Quit"<< endl;
+		cout << "Please make a selection (1,2, or 3): ";
+		cin >> selection;
+		
+		switch(selection)
+		{
+		case 1:
+			//print out pup stuff
+			break;
+		case 2:
+			//interaction stuff
+			break;
+		case 3:
+			isPlaying = false;
+		}
+	}while(isPlaying);
+
 }
